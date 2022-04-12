8aba107592df33ec7322a994ac8e9773baedd33e
Christian Grey <truongthanhhung2k2@gmail.com>
2022-04-12 22:04:51 +0700

    Update

--- Social_Network_BE/app.js
+++ Social_Network_BE/app.js
@@ -4,7 +4,14 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+// 
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,7 +27,12 @@ app.use(router)
 mongoose.connect('mongodb://localhost/social_network_dev')
     .then(result => {
         console.log('connect database successfully');
-        app.listen(PORT, () => {
+
+        io.on('connection', (socket) => {
+            console.log('user conneciton')
+        })
+
+        httpServer.listen(PORT, () => {
             console.log('server is listening on port ' + PORT)
         })
     })
