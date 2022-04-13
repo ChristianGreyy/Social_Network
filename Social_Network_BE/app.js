8dc102c0f5ef2816565c6403db0436c05660f737
Christian Grey <truongthanhhung2k2@gmail.com>
2022-04-13 14:47:11 +0700

    Update

--- Social_Network_BE/app.js
+++ Social_Network_BE/app.js
@@ -13,7 +13,7 @@ const io = new Server(httpServer, { /* options */ });
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(logger('dev'));
 app.use(express.json());
