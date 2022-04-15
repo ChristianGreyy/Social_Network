7f231d6b0b32a7b9034c55cde21f27372557a442
Christian Grey <truongthanhhung2k2@gmail.com>
2022-04-15 17:08:42 +0700

    Update

--- Social_Network_BE/models/user.model.js
+++ Social_Network_BE/models/user.model.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 
 const userSchema = new Schema({
-    username: {
+    email: {
         type: String,
         trim: true,
         unique: true,
